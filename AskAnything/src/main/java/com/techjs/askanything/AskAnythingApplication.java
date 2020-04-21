4ed5ff7507fff02581575b2a2b2b1f974f2dbd18
Sachin Singh <sachinsingh.sk13@gmail.com>
2020-04-21 17:44:44 +0530

    initial commit

---
+++ AskAnything/src/main/java/com/techjs/askanything/AskAnythingApplication.java
@@ -0,0 +1,13 @@
+package com.techjs.askanything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskAnythingApplication.class, args);
+	}
+
+}
---
+++ AskAnything/src/test/java/com/techjs/askanything/AskAnythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techjs.askanything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskAnythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
